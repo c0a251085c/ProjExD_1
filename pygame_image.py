c2abf6f7916668d5eec5bffd772fe2501ddc7b15
c0a251085c <c0a251085@edu.teu.ac.jp>
2026-06-09 16:44:50 +0900

    練習10：こうかとんのキー移動

--- pygame_image.py
+++ pygame_image.py
@@ -16,6 +16,9 @@ def main():
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
 
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 300, 200
+
     tmr = 0
 
     while True:
@@ -29,7 +32,18 @@ def main():
         screen.blit(bg_img_flip, [-x + 1600, 0])
         screen.blit(bg_img, [-x + 3200, 0])
 
-        screen.blit(kk_img, [300, 200])
+        key_lst = pg.key.get_pressed()
+
+        if key_lst[pg.K_UP]:
+            kk_rct.move_ip(0, -1)
+        if key_lst[pg.K_DOWN]:
+            kk_rct.move_ip(0, 1)
+        if key_lst[pg.K_LEFT]:
+            kk_rct.move_ip(-1, 0)
+        if key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip(1, 0)
+
+        screen.blit(kk_img, kk_rct)
 
         pg.display.update()
         tmr += 1
@@ -41,4 +55,3 @@ if __name__ == "__main__":
     main()
     pg.quit()
     sys.exit()
-    
